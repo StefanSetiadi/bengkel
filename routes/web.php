cbb6938e2f2501087c80add6ba3b182f163ba081
Stefan Setiadi <stefansetiadi452002@gmail.com>
2024-07-16 18:00:49 +0700

    [+] clean code

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,104 @@ use App\Http\Controllers\PaymentController;
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+|                                   Customer
+|--------------------------------------------------------------------------
+*/
+// Homepage
+Route::get('/', [ViewController::class, 'indexView'])->name('index');
+Route::get('/about-us', [ViewController::class, 'aboutView'])->name('about-us');
+
+// Login
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('customer');
+
+// Register
+Route::get('register', [RegisterController::class, 'register'])->name('register');
+Route::post('register/action', [RegisterController::class, 'actionregister'])->name('actionregister');
+
+// Shop
+Route::get('/shop', [ShopController::class, 'shopView'])->name('shop');
+Route::get('/shop-details{id_sparepart}', [ShopController::class, 'shopDetailsView'])->name('shop-details');
+Route::get('/view-cart', [ShopController::class, 'viewCart'])->name('view-cart')->middleware('customer');
+Route::post('/removeCart', [ShopController::class, 'removeCart'])->name('removeCart')->middleware('customer');
+
+Route::post('/actionCheckout', [ShopController::class, 'actionCheckout'])->name('actionCheckout')->middleware('customer');
+Route::post('/addCart', [ShopController::class, 'addCart'])->name('addCart')->middleware('customer');
+Route::post('/editCart', [ShopController::class, 'editCart'])->name('editCart')->middleware('customer');
+
+// Service
+Route::get('/service', [ServiceController::class, 'serviceView'])->name('service')->middleware('customer');
+Route::get('/service-details{id_sparepart}', [ServiceController::class, 'serviceDetailsView'])->name('service-details')->middleware('customer');
+
+// Transaction
+Route::post('/removeTransaction', [ShopController::class, 'removeTransaction'])->name('removeTransaction')->middleware('customer');
+
+// Booking
+Route::get('/booking', [BookingController::class, 'bookingView'])->name('booking')->middleware('customer');
+Route::post('/addBooking', [BookingController::class, 'addBooking'])->name('addBooking')->middleware('customer');
+Route::post('/removeBooking', [BookingController::class, 'removeBooking'])->name('removeBooking')->middleware('customer');
+Route::post('/acceptBooking', [BookingController::class, 'acceptBooking'])->name('acceptBooking')->middleware('customer');
+Route::post('/rejectBooking', [BookingController::class, 'rejectBooking'])->name('rejectBooking')->middleware('customer');
+
+// History
+Route::get('/historyBooking', [BookingController::class, 'historyBooking'])->name('historyBooking')->middleware('customer');
+Route::get('/historyTransaction', [PaymentController::class, 'historyTransaction'])->name('historyTransaction')->middleware('customer');
+Route::post('/detailHistoryTransaction', [PaymentController::class, 'detailHistoryTransaction'])->name('detailHistoryTransaction')->middleware('customer');
+Route::get('/historyServices', [ServiceController::class, 'historyServices'])->name('historyServices')->middleware('customer');
+Route::post('/detailHistoryService', [PaymentController::class, 'detailHistoryService'])->name('detailHistoryService')->middleware('customer');
+
+// PDF
+Route::get('history/transaction/invoice/{idTransaction}/generate', [PaymentController::class, 'generateInvoiceTransaction'])->name('generateInvoiceTransaction')->middleware('customer');
+Route::get('history/service/invoice/{idService}/generate', [PaymentController::class, 'generateInvoiceService'])->name('generateInvoiceService')->middleware('customer');
+
+
+
+/*
+|--------------------------------------------------------------------------
+|                                   Admin
+|--------------------------------------------------------------------------
+*/
+// Booking
+Route::get('/bookingDashboard', [BookingController::class, 'bookingDashboardView'])->name('bookingDashboard');
+
+// Transaction
+Route::get('/dashboardTransaction', [PaymentController::class, 'dashboardTransaction'])->name('dashboardTransaction');
+
+// Services
+Route::get('/servicesDashboard', [ServiceController::class, 'servicesDashboardView'])->name('servicesDashboard');
+Route::post('/createBill', [ServiceController::class, 'createBill'])->name('createBill');
+Route::post('/setServiceFee', [ServiceController::class, 'setServiceFee'])->name('setServiceFee');
+Route::post('/addSparepartService', [ServiceController::class, 'addSparepartService'])->name('addSparepartService');
+Route::post('/addDataService', [ServiceController::class, 'addDataService'])->name('addDataService');
+Route::post('/setQuantitySparepartService', [ServiceController::class, 'setQuantitySparepartService'])->name('setQuantitySparepartService');
+Route::post('/deleteSparepartService', [ServiceController::class, 'deleteSparepartService'])->name('deleteSparepartService');
+
+
+// Sparepart
+Route::get('/spareparts', [SparepartController::class, 'sparepartsView'])->name('spareparts');
+Route::get('/createSparepart', [SparepartController::class, 'createView'])->name('createSparepart');
+Route::get('/editSparepart{id_sparepart}', [SparepartController::class, 'editView'])->name('editSparepart');
+Route::get('/searchSparepart', [SparepartController::class, 'createView'])->name('searchSparepart');
+
+Route::post('/addSparepart', [SparepartController::class, 'addSparepart'])->name('addSparepart');
+Route::post('/editDataSparepart', [SparepartController::class, 'editDataSparepart'])->name('editDataSparepart');
+Route::post('/deleteDataSparepart', [SparepartController::class, 'deleteDataSparepart'])->name('deleteDataSparepart');
+
+
+// Payment
+Route::post('/updateTransaction', [PaymentController::class, 'updateTransaction'])->name('updateTransaction');
+Route::post('/updateService', [PaymentController::class, 'updateService'])->name('updateService');
+
+
+
+/*
+|--------------------------------------------------------------------------
+|                                   Unused
+|--------------------------------------------------------------------------
+*/
 Route::view('/blog-details', 'blog-details')->name('blog-details');
 Route::view('/blog', 'blog')->name('blog');
 Route::view('/checkout', 'checkout')->name('checkout');
@@ -51,84 +149,3 @@ Route::view('/reports', 'dashboard/reports')->name('reports');
 Route::view('/reviews', 'dashboard/reviews')->name('reviews');
 Route::view('/signup', 'dashboard/signup')->name('signup');
 Route::view('/tables', 'dashboard/tables')->name('tables');
-
-// Login
-Route::get('/login', [LoginController::class, 'login'])->name('login');
-Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
-Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
-
-// Register
-Route::get('register', [RegisterController::class, 'register'])->name('register');
-Route::post('register/action', [RegisterController::class, 'actionregister'])->name('actionregister');
-
-
-
-// View
-Route::get('/', [ViewController::class, 'indexView'])->name('index');
-Route::get('/about-us', [ViewController::class, 'aboutView'])->name('about-us');
-
-// Service
-Route::get('/service', [ServiceController::class, 'serviceView'])->name('service')->middleware('auth');
-Route::get('/service-details{id_sparepart}', [ServiceController::class, 'serviceDetailsView'])->name('service-details')->middleware('auth');
-
-
-// Shop
-Route::get('/shop', [ShopController::class, 'shopView'])->name('shop');
-Route::get('/shop-details{id_sparepart}', [ShopController::class, 'shopDetailsView'])->name('shop-details');
-Route::get('/view-cart', [ShopController::class, 'viewCart'])->name('view-cart')->middleware('auth');
-Route::post('/removeCart', [ShopController::class, 'removeCart'])->name('removeCart')->middleware('auth');
-
-Route::post('/actionCheckout', [ShopController::class, 'actionCheckout'])->name('actionCheckout')->middleware('auth');
-Route::post('/addCart', [ShopController::class, 'addCart'])->name('addCart')->middleware('auth');
-Route::post('/editCart', [ShopController::class, 'editCart'])->name('editCart')->middleware('auth');
-
-// Transaction
-Route::post('/removeTransaction', [ShopController::class, 'removeTransaction'])->name('removeTransaction')->middleware('auth');
-
-// Booking
-Route::get('/booking', [BookingController::class, 'bookingView'])->name('booking')->middleware('auth');
-Route::post('/addBooking', [BookingController::class, 'addBooking'])->name('addBooking')->middleware('auth');
-Route::post('/removeBooking', [BookingController::class, 'removeBooking'])->name('removeBooking')->middleware('auth');
-
-Route::post('/acceptBooking', [BookingController::class, 'acceptBooking'])->name('acceptBooking')->middleware('auth');
-Route::post('/rejectBooking', [BookingController::class, 'rejectBooking'])->name('rejectBooking')->middleware('auth');
-
-Route::get('/bookingDashboard', [BookingController::class, 'bookingDashboardView'])->name('bookingDashboard');
-
-// History
-Route::get('/historyBooking', [BookingController::class, 'historyBooking'])->name('historyBooking')->middleware('auth');
-Route::get('/historyTransaction', [PaymentController::class, 'historyTransaction'])->name('historyTransaction')->middleware('auth');
-Route::post('/detailHistoryTransaction', [PaymentController::class, 'detailHistoryTransaction'])->name('detailHistoryTransaction')->middleware('auth');
-Route::get('/historyServices', [ServiceController::class, 'historyServices'])->name('historyServices')->middleware('auth');
-Route::post('/detailHistoryService', [PaymentController::class, 'detailHistoryService'])->name('detailHistoryService')->middleware('auth');
-
-Route::get('/dashboardTransaction', [PaymentController::class, 'dashboardTransaction'])->name('dashboardTransaction');
-
-// Services
-Route::get('/servicesDashboard', [ServiceController::class, 'servicesDashboardView'])->name('servicesDashboard');
-Route::post('/createBill', [ServiceController::class, 'createBill'])->name('createBill');
-Route::post('/setServiceFee', [ServiceController::class, 'setServiceFee'])->name('setServiceFee');
-Route::post('/addSparepartService', [ServiceController::class, 'addSparepartService'])->name('addSparepartService');
-Route::post('/addDataService', [ServiceController::class, 'addDataService'])->name('addDataService');
-Route::post('/setQuantitySparepartService', [ServiceController::class, 'setQuantitySparepartService'])->name('setQuantitySparepartService');
-Route::post('/deleteSparepartService', [ServiceController::class, 'deleteSparepartService'])->name('deleteSparepartService');
-
-
-// Sparepart
-Route::get('/spareparts', [SparepartController::class, 'sparepartsView'])->name('spareparts');
-Route::get('/createSparepart', [SparepartController::class, 'createView'])->name('createSparepart');
-Route::get('/editSparepart{id_sparepart}', [SparepartController::class, 'editView'])->name('editSparepart');
-Route::get('/searchSparepart', [SparepartController::class, 'createView'])->name('searchSparepart');
-
-Route::post('/addSparepart', [SparepartController::class, 'addSparepart'])->name('addSparepart');
-Route::post('/editDataSparepart', [SparepartController::class, 'editDataSparepart'])->name('editDataSparepart');
-Route::post('/deleteDataSparepart', [SparepartController::class, 'deleteDataSparepart'])->name('deleteDataSparepart');
-
-// PDF
-Route::get('history/transaction/invoice/{idTransaction}/generate', [PaymentController::class, 'generateInvoiceTransaction'])->name('generateInvoiceTransaction');
-Route::get('history/service/invoice/{idService}/generate', [PaymentController::class, 'generateInvoiceService'])->name('generateInvoiceService');
-
-// Payment
-Route::get('/payment', [PaymentController::class, 'payment'])->name('payment');
-Route::post('/updateTransaction', [PaymentController::class, 'updateTransaction'])->name('updateTransaction');
-Route::post('/updateService', [PaymentController::class, 'updateService'])->name('updateService');
